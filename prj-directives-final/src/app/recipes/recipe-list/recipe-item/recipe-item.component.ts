e8761b40e34af67b0feb31c49238369272f93cec
dnduc <ducdn221@gmail.com>
2020-03-22 22:21:07 +0700

    add router into project

--- prj-directives-final/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ prj-directives-final/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from '../../recipe.model';
 import { RecipeService } from '../../recipe.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-item',
@@ -11,13 +12,6 @@ import { RecipeService } from '../../recipe.service';
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor(private recipeService: RecipeService) { }
-
   ngOnInit() {
   }
-
-  onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
-
 }
--- prj-directives-final/src/app/recipes/recipe.service.ts
+++ prj-directives-final/src/app/recipes/recipe.service.ts
@@ -7,11 +7,11 @@ import { ShoppingListService } from "../shopping-list/shopping-list.service";
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', [
+        new Recipe(1, 'A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', [
             new Ingredient('Meat', 1),
             new Ingredient('French Fries', 20)
         ]),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', [
+        new Recipe(2, 'Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', [
             new Ingredient('Buns', 2),
             new Ingredient('Meat', 1)
         ])
@@ -20,6 +20,10 @@ export class RecipeService {
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    getRecipe(id: number) {
+        return this.recipes.find(item => item.id === id);
     }
 
     addIngredientToShoppingList(ingredients: Ingredient[]) {
